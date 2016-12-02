3f66edfc2d31791e5f1c6fbeb00add6a25636c97
Theron Lewis <theronl@gmail.com>
2016-12-02 15:30:00 -0700

    Added forula for installing older maven version.

---
+++ Formula/maven30.rb
@@ -0,0 +1,22 @@
+class Maven30 < Formula
+  desc "Java-based project management"
+  homepage "https://maven.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz"
+  sha256 "d98d766be9254222920c1d541efd466ae6502b82a39166c90d65ffd7ea357dd9"
+
+  depends_on :java
+
+  conflicts_with "maven", :because => "Differing versions of same formula"
+
+  def install
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
+
+    # Fix the permissions on the global settings file.
+    chmod 0644, "conf/settings.xml"
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# homebrew-tap
+# Homebrew formulas
+
+## Installation
+
+To install any formula from this repository you can do:
+
+```
+brew install sobolevn/tap/FORMULA
+```
+
+There's also an option to use `brew tap`:
+
+```
+brew tap sobolevn/tap
+brew install FORMULA
+```
+
+## List of formulas:
+
+* maven30 - install older version of maven
+
